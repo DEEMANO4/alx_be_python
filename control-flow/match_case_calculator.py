a0189f1d9244e59746486fff2d9dce83e12d7a41
DEEMANO4 <apenadavid18@gmail.com>
2025-09-18 21:50:04 +0100

    calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,19 +3,16 @@ num2_prompt = input("Enter the second number:")
 num1 = int(num1_prompt)
 num2 = int(num2_prompt)
 operator = input("Choose the operation (+,-,*,/):")
-Addition = num1 + num2
-Subtraction = num1 - num2
-Multiplication = num1 * num2
-
-
-
 
 match operator:
     case "+":
+        Addition = num1 + num2
         print("The result is", Addition)
     case "-":
+        Subtraction = num1 - num2
         print("The result is", Subtraction)
     case "*":
+        Multiplication = num1 * num2
         print("The result is", Multiplication)
     case "/":
         if num2 != 0:
