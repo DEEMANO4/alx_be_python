0d8f8090d19648db7571fdd786c2a3f1e89e2081
DEEMANO4 <apenadavid18@gmail.com>
2025-10-03 19:16:56 +0100

    ..

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,7 +20,41 @@ class TestSimpleCalculator(unittest.TestCase):
         # Add more assertions to thoroughly test the add method.
 
 
-    #def test_subtracion(self):
-    #    """Test the subtraction method."""
+    def test_subtracion(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-5, -2), -3)
+        self.assertEqual(self.calc.subtract(7, 0), 7)
+        self.assertEqual(self.calc.subtract(0, -5), 5)
+        self.assertEqual(self.calc.subtract(4, -1), 5)
+        self.assertEqual(self.calc.subtract(2.5, 3.5), 1)
+        self.assertEqual(self.calc.subtract(0.1, 0.2), -0.1)
+
+
+    def test_multiplication(self):
+        """Test the multiplication method"""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-5, -2), 10)
+        self.assertEqual(self.calc.multiply(7, 0), 0)
+        self.assertEqual(self.calc.multiply(0, -5), 0)
+        self.assertEqual(self.calc.multiply(4, -1), -4)
+        self.assertEqual(self.calc.multiply(2.5, 3.5), 8.75)
+        self.assertEqual(self.calc.multiply(0.1, 0.2), 0.02)
+
+
+    def test_division(self):
+        """Test the division method"""
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(2, 3), 0.66)
+        self.assertEqual(self.calc.divide(-1, 1), -1)
+        self.assertEqual(self.calc.divide(-5, -2), 2.5)
+        self.assertEqual(self.calc.divide(7, 0), 0)
+        self.assertEqual(self.calc.divide(0, -5), 0)
+        self.assertEqual(self.calc.divide(4, -1), -4)
+        self.assertEqual(self.calc.divide(2.5, 3.5), 0.71)
+        self.assertEqual(self.calc.divide(0.1, 0.2), 0.5)
+
         
 # Remember to write additional test methods for subtract, multiply, and divide.
