acb5105b2bfc64c718ee88faf4cac0476ddeb1e7
DEEMANO4 <apenadavid18@gmail.com>
2025-10-03 14:55:50 +0100

    ..

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,8 @@
+def safe_divide(numerator, denominator):
+    try:
+        print( numerator / denominator)
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+
+    except ValueError:
+        print("Error: Please enter numeric values only")
