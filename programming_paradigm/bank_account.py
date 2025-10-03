4aca61c3a9d78ce2dea5437b20d975ceb93b4ebb
DEEMANO4 <apenadavid18@gmail.com>
2025-10-03 10:37:14 +0100

    trial

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,22 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = account_balance
+    def __init__(self, Current_Balance):
+        self.account_balance = Current_Balance
 
 
     def deposit(self, amount):
+        self.account_balance += amount
+        #print(self.account_balance)
+
+
+    def withdraw(self, amount):
         if amount > 0:
-            self.account_balance = self.account_balance + amount
-            print(self.account_balance)
-
-
-    def withdrawal(self, amount):
-        if amount <= self.account_balance:
-            self.account_balance = self.account_balance - amount
+            if amount < self.account_balance:
+                self.account_balance = self.account_balance - amount
+                return self.account_balance
         
 
     def display_balance(self):
+       ## return self.account_balance
         print(self.account_balance)
     
 
