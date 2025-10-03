1cbb67dfafe3c8a89081a26311bcc3a1c709c1d5
DEEMANO4 <apenadavid18@gmail.com>
2025-10-03 12:29:18 +0100

    ?

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,22 @@
 class BankAccount:
-    def __init__(self, Current_Balance):
-        self.account_balance = Current_Balance
+    def __init__(self, account_Balance=0):
+        self.account_balance = account_Balance
 
 
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount > 0:
+            self.account_balance += amount
+        #return self.account_balance
         #print(self.account_balance)
 
 
     def withdraw(self, amount):
         if amount > 0:
             if amount < self.account_balance:
-                self.account_balance = self.account_balance - amount
-                return self.account_balance
-        
+                self.account_balance -= amount
+                
 
     def display_balance(self):
-       ## return self.account_balance
-        print("Current Balance:", self.account_balance)
+        print(f"Current Balance: ${self.account_balance}")
     
 
