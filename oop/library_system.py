50382b6f606c0b234c3b4086e851d55f7d143b68
DEEMANO4 <apenadavid18@gmail.com>
2025-10-10 17:34:12 +0100

    ..

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,10 @@
 class Book:
-    def __init__(self, title, author, ):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        pass
+        return f"Title: {self.title}, Author: {self.author}"
 
 
 
@@ -13,10 +13,16 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return super().__str__()
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return super().__str__()
 
 
 class Library:
